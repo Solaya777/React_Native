cb1ad642009108fbde3d32e867606d42e0f11e17
Solaya777 <solaya88@soy.sena.edu.co>
2024-08-26 23:02:19 -0500

    arreglos css

--- App.js
+++ App.js
@@ -10,44 +10,53 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import appFirebase from './Credenciales';
 import { getAuth } from 'firebase/auth';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Text, Image, Linking, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
+
+
 const auth = getAuth(appFirebase);
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{ headerTitle: "Registro" }}
-        />
-
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerTitle: "Home" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ headerTitle: "Registro" }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerTitle: "Home" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
 function LoginScreen() {
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+    style={styles.container}
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+  >
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
       <Header />
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <LoginForm />
-      </ScrollView>
-      <Footer />
-    </View>
+      <LoginForm />
+    </ScrollView>
+    <Footer />
+  </KeyboardAvoidingView>
   );
 }
 
--- components/Footer.js
+++ components/Footer.js
@@ -4,66 +4,58 @@ import { View, Text, StyleSheet, Image, Linking, TouchableOpacity } from 'react-
 export default function Footer() {
   return (
     <View style={styles.footer}>
-      <View style={styles.section}>
-        <Image source={require('../assets/images/LOGO.png')} style={styles.logo} />
-        <Text style={styles.aboutText}>
-          Tears of Misery Death Metal Latinoamericano, Fundación año 2003 Bogotá D.C. Colombia
-        </Text>
+      <Image source={require('../assets/images/LOGO.png')} style={styles.logo} />
+      <Text style={styles.aboutText}>
+        Tears of Misery Death Metal Latinoamericano, Fundación año 2003 Bogotá D.C. Colombia
+      </Text>
+      <View style={styles.socialLinks}>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.facebook.com/tearsofmisery')}>
+          <Text style={styles.socialLink}>Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.instagram.com/tearsof.misery/')}>
+          <Text style={styles.socialLink}>Instagram</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.youtube.com/channel/UCjcaDSYUyl_VjlU31vPeH6Q')}>
+          <Text style={styles.socialLink}>YouTube</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.section}>
-        <Text style={styles.followUs}>Síguenos</Text>
-        <View style={styles.socialLinks}>
-          <TouchableOpacity onPress={() => Linking.openURL('https://www.facebook.com/tearsofmisery')}>
-            <Text style={styles.socialLink}>Facebook</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Linking.openURL('https://www.instagram.com/tearsof.misery/')}>
-            <Text style={styles.socialLink}>Instagram</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Linking.openURL('https://www.youtube.com/channel/UCjcaDSYUyl_VjlU31vPeH6Q')}>
-            <Text style={styles.socialLink}>YouTube</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.section}>
-        <Text style={styles.copyRight}>
-          &copy; 2023 <Text style={styles.boldText}>Tears Of Misery</Text> - Todos los Derechos Reservados.
-        </Text>
-      </View>
+      <Text style={styles.copyRight}>
+        &copy; 2023 <Text style={styles.boldText}>Tears Of Misery</Text> - Todos los Derechos Reservados.
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   footer: {
-    padding: 16,
+    padding: 10,
     backgroundColor: '#fff',
     borderTopWidth: 1,
     borderTopColor: '#ccc',
-  },
-  section: {
-    marginBottom: 16,
+    alignItems: 'center',
   },
   logo: {
-    width: 100,
-    height: 40,
+    width: 80,
+    height: 30,
+    marginBottom: 8,
   },
   aboutText: {
-    fontSize: 16,
-    marginVertical: 8,
-  },
-  followUs: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: 12,
+    textAlign: 'center',
     marginBottom: 8,
   },
   socialLinks: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginBottom: 8,
   },
   socialLink: {
     color: '#007BFF',
+    fontSize: 14,
   },
   copyRight: {
+    fontSize: 10,
     textAlign: 'center',
   },
   boldText: {
